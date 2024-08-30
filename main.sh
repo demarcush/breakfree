b51554ad5e9014747c960862c8adb617535e30b6
demarcush <146051763+demarcush@users.noreply.github.com>
2024-08-30 13:53:07 +0000

    Add files via upload

--- main.sh
+++ main.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env sh
 export GOPROXY="https://proxy.golang.org,direct" GOAMD64='v3'
-export SRV_FILE=final
-
-function populate() {
-	cp template-cn.json template-${1}.json; patch template-${1}.json ${1}.json.patch
-	cp template-${1}.json template-${1}-sfw.json; cp template-${1}.json template-${1}-notun.json
-	patch template-${1}-sfw.json sfw.json.patch; patch template-${1}-notun.json notun.json.patch
-	cp template-${1}-sfw.json template-${1}-sfw-notun.json; patch template-${1}-sfw-notun.json notun.json.patch
-}
+export SRV=final
 
 function exp() {
-	python main.py ${SRV_FILE}.json templates/template-${1}.json release/Sing-Box/${1}-lite.json
-	python main.py ${SRV_FILE}.json templates/template-${1}-sfw.json release/Sing-Box/${1}-sfw-lite.json
-	python main.py ${SRV_FILE}.json templates/template-${1}-notun.json release/Sing-Box/${1}-lite-notun.json
-	python main.py ${SRV_FILE}.json templates/template-${1}-sfw-notun.json release/Sing-Box/${1}-sfw-lite-notun.json
+	python main.py ${SRV}.json templates/template-${1}.json release/Sing-Box/${1}-lite.json
+	python main.py ${SRV}.json templates/template-${1}-sfw.json release/Sing-Box/${1}-sfw-lite.json
+	python main.py ${SRV}.json templates/template-${1}-notun.json release/Sing-Box/${1}-lite-notun.json
+	python main.py ${SRV}.json templates/template-${1}-sfw-notun.json release/Sing-Box/${1}-sfw-lite-notun.json
 }
 
 git clone https://github.com/SagerNet/serenity
@@ -23,18 +16,11 @@ serenity -c serenity.json run &
 git clone https://github.com/SagerNet/sing-box
 cd sing-box && git checkout main-next && make install && cd ..
 
-curl -fsSL -o "${SRV_FILE}.json" http://localhost:8080/${SRV_FILE}
+curl -fsSL -o "${SRV}.json" http://localhost:8080/${SRV}
 
 #quick-fix
-sed -i s/\;mux\=true//g ${SRV_FILE}.json
-sed -i s/mux\=true\;//g ${SRV_FILE}.json
-
-#populating templates
-cd templates
-populate cn
-populate ir
-populate ru
-cd ..
+sed -i s/\;mux\=true//g ${SRV}.json
+sed -i s/mux\=true\;//g ${SRV}.json
 
 exp cn && echo "CN files exported!"
 exp ir && echo "IR files exported!"
